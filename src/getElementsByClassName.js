1bc213075c56abcc96589ce99cf7dfe904f4fd7c
Brian Hauck <brihauck4@gmail.com>
2020-08-17 15:21:18 -0700

    complete getElementsByClassName js and spec file to passing all test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
+  //console.log(document.children[0].children[0].length)
   // your code here
+  var targetClasses = [];
+  recurseTree(targetClasses, document.body, className);
+  return targetClasses;
+};
+
+const recurseTree = (targetClasses, node, className) => {
+
+  if (node.classList.contains(className)) {
+    targetClasses.push(node);
+  }
+
+  if (node.children.length !== undefined) {
+    console.log(node.children);
+    for (let i = 0; i < node.children.length; i++) {
+      let child = node.children[i];
+
+      if (child.children) {
+        recurseTree(targetClasses, child, className);
+      }
+    }
+  }
+
 };
