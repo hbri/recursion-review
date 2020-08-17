42d94396e9c63be0530bb6b84ab92e73d3412ba0
Brian Hauck <brihauck4@gmail.com>
2020-08-17 16:29:57 -0700

    update code for stringifyJSON and stringifyJSONspec to have working code pass all test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var strung = '';
+  if (typeof obj === 'string') {
+    strung += `"${obj}"`;
+  } else if (typeof obj === 'number') {
+    strung += obj;
+  } else if (typeof obj === 'boolean') {
+    strung += `${obj}`;
+  } else if (obj === null) {
+    strung += 'null';
+  } else if (obj === undefined) {
+    strung += 'undefined';
+  } else if (Array.isArray(obj)) {
+    strung += `[${obj.map(stringifyJSON)}]`;
+  } else if (typeof obj === 'object') {
+    strung += '{';
+    let keys = Object.keys(obj);
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] !== 'functions' && keys[i] !== 'undefined') {
+        strung += stringifyJSON(keys[i]);
+        strung += ':';
+        strung += stringifyJSON(obj[keys[i]]);
+        if (i !== keys.length - 1) {
+          strung += ',';
+        }
+      }
+
+    }
+    strung += '}';
+  }
+
+  return strung;
 };
